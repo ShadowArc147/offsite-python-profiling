b127a5f60e1e1224213a662dbc6cdfaf5cd3d0f1
Thomas.A <thomas.a@ncscmse.onmicrosoft.com>
2023-09-05 17:19:49 +0100

    Added fixed code

--- problem-1/super-quick-function.py
+++ problem-1/super-quick-function.py
@@ -1,20 +1,23 @@
 import re
+import time
+from tqdm import tqdm
 
 num_total_matches = 0
 pattern1 = r"[0-9]{1}help"
 pattern2 = r"[0-9]{1}me"
+start = time.time()
     
 print("finding file")
+
 with open('random.txt', 'rt') as f:
     print("Opened File")
     print("searching for patterns")
-    for line in f:
+    for line in tqdm(f, total=1_000_000):
         line = line.lower()  # make sure our text is all lowercase 
         for pattern in [pattern1, pattern2]:
             # Find all occurrences of the pattern in the string
             matches = re.findall(pattern, line)
-
-                # Count the number of matches
+            # Count the number of matches
             num_matches = len(matches)
             num_total_matches += num_matches
                 
@@ -22,4 +25,6 @@ with open('random.txt', 'rt') as f:
         print ("No Matches")
     else:
         print ("Pattern Matches =" + ' ' + str(num_total_matches))
+end = time.time()
+print("Time taken = " + str(end-start) + "s")
             
